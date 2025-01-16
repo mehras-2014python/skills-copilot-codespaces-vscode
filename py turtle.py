8e001b90798a2e26bcf45d8809cdd233979c347e
mehras <fahimeh.nabi1361@gmail.com>
2025-01-16 16:01:09 +0330

    an turtle Graphics very fun and beautiful!!!!

---
+++ py turtle.py
@@ -0,0 +1,18 @@
+import turtle as t
+turtle = t.Turtle()
+
+
+turtle.pensize(1)
+turtle.speed(40)
+turtle.color("black")
+
+
+turtle.fillcolor("blue")
+turtle.begin_fill()
+
+
+for i in range (134):
+    turtle.right(890)
+    turtle.forward(798)
+
+
